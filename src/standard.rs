5114713a346171479065d3d0313894c58ea2a225
DomiDre <homiedomi@gmail.com>
2019-07-28 19:10:11 +0200

    Shortened function defintion with a closure

--- src/standard.rs
+++ src/standard.rs
@@ -1,17 +1,9 @@
 use ndarray::Array1;
 
 pub fn linear(p: &Array1<f64>, x: &Array1<f64>) -> Array1<f64> {
-    let mut result: Vec<f64> = Vec::new();
-    for xval in x.iter() {
-        result.push(p[0] * xval + p[1]);
-    }
-    Array1::from(result)
+    x.map( |xval| p[0] * xval + p[1])
 }
 
 pub fn parabola(p: &Array1<f64>, x: &Array1<f64>) -> Array1<f64> {
-    let mut result: Vec<f64> = Vec::new();
-    for xval in x.iter() {
-        result.push(p[0] * xval * xval + p[1] * xval + p[2]);
-    }
-    Array1::from(result)
+    x.map( |xval| p[0] * xval * xval + p[1] * xval + p[2])
 }
