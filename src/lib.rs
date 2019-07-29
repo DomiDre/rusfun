13d7c2afa5b47b69b4f3b217e11d30518a1ef47a
DomiDre <homiedomi@gmail.com>
2019-07-29 21:21:46 +0200

    Add some modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,10 @@
 #![allow(dead_code)]
 
-mod func1d;
-pub use crate::func1d::Func1D;
-
 mod standard;
+mod utils;
+mod func1d;
+mod lm;
+
 pub use crate::standard::parabola;
+pub use crate::func1d::Func1D;
+pub use crate::lm::residuum;
