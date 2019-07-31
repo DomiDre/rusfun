ade6aacfce2a978d5edd2aa7442937bf8c278d96
DomiDre <homiedomi@gmail.com>
2019-07-31 19:42:46 +0200

    Add parameter_gradient, call for varying parameters, and add lifetimes

--- src/func1d.rs
+++ src/func1d.rs
@@ -1,21 +1,46 @@
-use ndarray::Array1;
+use ndarray::{Array1, Array2, Array, s};
 
-pub struct Func1D {
-    pub parameters: Array1<f64>,
-    pub domain: Array1<f64>,
+pub struct Func1D<'a> {
+    pub parameters: &'a Array1<f64>,
+    pub domain: &'a Array1<f64>,
     pub function: fn(&Array1<f64>, &Array1<f64>) -> Array1<f64>
 }
 
-impl Func1D {
-    pub fn new(parameters: Vec<f64>, domain: Vec<f64>, function: fn(&Array1<f64>, &Array1<f64>) -> Array1<f64>) -> Func1D {
+impl<'a> Func1D<'a> {
+    pub fn new(
+    parameters: &'a Array1<f64>,
+    domain: &'a Array1<f64>,
+    function: fn(&Array1<f64>, &Array1<f64>) -> Array1<f64>) -> Func1D<'a> {
         Func1D {
-            parameters: Array1::from(parameters),
-            domain: Array1::from(domain),
+            parameters: &parameters,
+            domain: &domain,
             function
         }
     }
 
     pub fn output(&self) -> Array1<f64> {
         (self.function)(&self.parameters, &self.domain)
+    }
+
+    pub fn for_parameters(&self, parameters: &Array1<f64>) -> Array1<f64> {
+        (self.function)(&parameters, &self.domain)
+    }
+
+    pub fn parameter_gradient(&self) -> Array2<f64> {
+        let epsilon = std::f64::EPSILON.sqrt();
+        let mut jacobian: Array2<f64> = Array::zeros((self.parameters.len(), self.domain.len()));
+
+        let func_values = self.output();
+        for i in 0..self.parameters.len() {
+            let mut shifted_parameters = self.parameters.clone();
+            let mut shift = epsilon*shifted_parameters[i].abs();
+            if shift == 0.0 { shift = epsilon };
+            shifted_parameters[i] += shift;
+            let shifted_func_values = self.for_parameters(&shifted_parameters);
+            let mut row_slice = jacobian.slice_mut(s![i, ..]);
+            let derivative: Array1<f64> = (shifted_func_values - func_values.clone()) / shift;
+            row_slice.assign(&derivative);
+        }
+        jacobian
     }
 }
