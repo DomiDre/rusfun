885f47ab0a1d162d672bfade212a90bf09b2e15d
DomiDre <homiedomi@gmail.com>
2019-08-03 20:58:53 +0200

    Add metrics for convergence

--- src/curve_fit.rs
+++ src/curve_fit.rs
@@ -15,7 +15,10 @@ pub struct MinimizationStep {
 	parameters: Array1<f64>,
 	residuum:  Array1<f64>,
 	chi2: f64,
-	metric: f64
+	redchi2: f64,
+	metric: f64,
+	metric_gradient: f64,
+	metric_parameters: f64,
 }
 
 pub struct Minimizer<'a> {
@@ -27,6 +30,9 @@ pub struct Minimizer<'a> {
 	pub num_func_evaluation: usize,
 	pub residuum: Array1<f64>,
 	pub chi2: f64,
+	pub dof: usize,
+	pub redchi2: f64,
+	pub convergence_message: &'a str,
 	pub epsilon1: f64,
 	pub epsilon2: f64,
 	pub epsilon3: f64,
@@ -42,6 +48,8 @@ impl<'a> Minimizer<'a> {
 		let initial_parameters = model.parameters.clone();
 		let initial_residuum = std_residuum(&model, &initial_parameters, &y, &sy);
 		let chi2 = chi2(&initial_residuum);
+		let dof = model.domain.len() - initial_parameters.len();
+		let redchi2 = chi2 / (dof as f64);
 		Minimizer {
 			model: &model,
 			y: &y,
@@ -51,6 +59,9 @@ impl<'a> Minimizer<'a> {
 			num_func_evaluation: 0,
 			residuum: initial_residuum,
 			chi2: chi2,
+			dof: dof,
+			redchi2: redchi2,
+			convergence_message: "",
 			epsilon1: 1e-3,
 			epsilon2: 1e-3,
 			epsilon3: 1e-1,
@@ -62,18 +73,6 @@ impl<'a> Minimizer<'a> {
 
 	pub fn residuum(&self, parameters: &Array1<f64>) -> Array1<f64> {
 		std_residuum(&self.model, &parameters, &self.y, &self.sy)
-	}
-
-	pub fn dof(&self) -> usize {
-		let n_params = self.model.parameters.len();
-		let n_points = self.model.domain.len();
-		n_points - n_params
-	}
-
-	pub fn redchi2(&self, residuum: &Array1<f64>) -> f64 {
-		let chi2 = chi2(residuum);
-		let dof = self.dof();
-		chi2 / (dof as f64)
 	}
 
 	pub fn lm(&mut self) -> MinimizationStep {
@@ -91,8 +90,8 @@ impl<'a> Minimizer<'a> {
 
 		// J^T is cloned to be multiplied by W later
 		let mut jt = j.clone().reversed_axes();
-		// calculate J^T W (y - f) (rhs), where W (y - f) is just the residuum
 		
+		// calculate J^T W (y - f) (rhs), where W (y - f) is just the residuum
 		let b = jt.dot(&self.residuum);
 
 		// multiply J^T with W to obtain J^T W
@@ -119,15 +118,24 @@ impl<'a> Minimizer<'a> {
 			metric +=  delta[i].powi(2)*gaussNewtonDiagonal[i];
 		}
 
+		// take maximum of the absolute value in the respective arrays as metric for the
+		// convergence of either the gradient or the parameters
+		let metric_gradient = b.map(|x| x.abs()).to_vec().iter().cloned().fold(0./0., f64::max);
+		let metric_parameters = (&delta / &self.minimizer_parameters).map(|x| x.abs()).to_vec().iter().cloned().fold(0./0., f64::max);;
+
 		let updated_parameters = self.minimizer_parameters.clone() + delta;
 		let updated_residuum = self.residuum(&updated_parameters);
 		let updated_chi2 = chi2(&updated_residuum);
+		let redchi2 = updated_chi2 / (self.dof as f64);
 
 		MinimizationStep {
 			parameters: updated_parameters,
 			residuum: updated_residuum,
 			chi2: updated_chi2,
-			metric: metric
+			redchi2: redchi2,
+			metric: metric,
+			metric_gradient: metric_gradient,
+			metric_parameters: metric_parameters
 		}
 		
 	}
@@ -148,16 +156,41 @@ impl<'a> Minimizer<'a> {
 				self.minimizer_parameters = update_step.parameters; 
 				self.residuum = update_step.residuum;
 				self.chi2 = update_step.chi2;
+				self.redchi2 = update_step.redchi2;
 
+				// check convergence criteria
+				// gradient converged
+				if update_step.metric_gradient < self.epsilon1 { 
+					self.convergence_message = "Gradient converged";
+					break 
+				};
+
+				// parameters converged
+				if update_step.metric_parameters < self.epsilon2 {
+					self.convergence_message = "Parameters converged";
+					break
+				};
+
+				// chi2 converged
+				if update_step.redchi2 < self.epsilon3 {
+					self.convergence_message = "Chi2 converged";
+					break
+				};
 			} else {
 				// new chi2 not good enough, increasing lambda
 				self.lambda = (self.lambda*self.lambda_UP_fac).min(1e7);
 			}
 		}
+		if iterations >= max_iterations {
+			self.convergence_message = "Reached max. number of iterations";
+		}
 	}
 
 	pub fn report(&self) {
+		println!("\t #Chi2:\t{}", self.chi2);
+		println!("\t #Red. Chi2:\t{}", self.redchi2);
 		println!("\t #Func. Evaluations:\t{}", self.num_func_evaluation);
+		println!("\t #Converged by:\t{}", self.convergence_message);
 		println!("---- Parameters ----");
 		for i in 0..self.minimizer_parameters.len() {
 			println!("{}\t(init: {:?})", self.minimizer_parameters[i], self.model.parameters[i]);
