f62e2ad93fd1c448b480684c797b8dac819351f6
DomiDre <homiedomi@gmail.com>
2019-08-05 17:37:43 +0200

    Add gaussian, simplify wasm fn call structure, clean repo

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,7 @@
 #![allow(non_snake_case)]
 pub mod curve_fit;
 pub mod func1d;
+pub mod size_distribution;
 pub mod standard;
 pub mod utils;
 pub mod wasm;
-
-pub use crate::wasm::*;
-
-pub use crate::curve_fit::Minimizer;
-pub use crate::func1d::Func1D;
-pub use crate::standard::parabola;
-pub use crate::utils::{matrix_solve, LU_decomp};
